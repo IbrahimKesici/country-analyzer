ab4975e00911196be4e58a985a17546d7bc07625
Kesici <Seyma.Kesici@dnv.com>
2022-08-06 11:14:04 +0200

    PR changes

--- country_code_scrapper.py
+++ country_code_scrapper.py
@@ -1,6 +1,5 @@
 from urllib import response
 import requests
-import pprint
 import json
 import re
 
@@ -9,25 +8,8 @@ from bs4 import BeautifulSoup
 response = requests.get("http://www.salaryexplorer.com/salary-survey.php")
 soup = BeautifulSoup(response.text, "html.parser")
 
-
-# print(soup.prettify)
-
-countries_to_check = ["Afghanistan","Algeria"]
 code_list = {}
-
-# for country in countries_to_check:
-#     table = soup.select(f'div.countryselection > b > a')
-#     for row in table:
-#         if row.get_text() == country.capitalize():
-#             location_code = row.get('href').split("?loc=",2)[1].split("&")[0]
-#             code_list[country] = {"location_code":location_code}
-       
-
-# print(code_list)
-
-# with open('data.json', 'w') as fp:
-#     json.dump(code_list, fp)
-            
+         
 country_div = soup.find("div", {"class": "countryselection"})
 for b_tag in country_div.find_all('b'):
     country_name = b_tag.get_text()
@@ -35,5 +17,5 @@ for b_tag in country_div.find_all('b'):
     location_code = re.findall("(?<=loc=)(.*)(?=&)", country_href)
     code_list[country_name] = {"location_code":location_code[0]}
    
-with open('data.json', 'w') as fp:
+with open('./config/countries.json', 'w') as fp:
     json.dump(code_list, fp)
